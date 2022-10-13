1762aef6804998203c0ad93215858c466376e85c
MikeyAllen <mikeyallen54@gmail.com>
2022-10-13 13:50:08 -0400

    updated

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from datetime import date
 from urllib import request
 from flask import Flask, render_template, request
+import math
 
 
 app = Flask(__name__)
 global studentOrganisationDetails
 # Assign default 5 values to studentOrganisationDetails for Application  3.
-
+studentOrganisationDetails = {}
 
 @app.route('/')
 def index():
@@ -18,34 +19,50 @@ def index():
 @app.get('/calculate')
 def displayNumberPage():
     # Complete this function to display form.html page
-    pass
+    return render_template('form.html')
 
 
 @app.route('/calculate', methods=['POST'])
 def checkNumber():
     # Get Number from form and display message according to number
-    # Display "Number {Number} is even" if given number is even on result.html page
-    # Display "Number {Number} is odd" if given number is odd on result.html page
-    # Display "No number provided" if value is null or blank on result.html page
-    # Display "Provided input is not an integer!" if value is not a number on result.html page
     global number
+    global results
+    global null
+    null = ' '
+    # Display "Number {Number} is even" if given number is even on result.html page
     number = request.form['number']
-    # if (number/2==0)
-
+    if (int(number)%2==0):
+        results="Number "+number+" is even"
+        return render_template('result.html', results=results)
+       # Display "Number {Number} is odd" if given number is odd on result.html page
+    elif (int(number)%2==1):
+        results="Number "+number+" is odd"
+        return render_template('result.html', results=results)
+    # Display "No number provided" if value is null or blank on result.html page
+    elif (number== null):
+        results="Provided input is not an integer!"
+        return render_template('result.html', results=results)
+    # Display "" if value is not a number on result.html page
+    elif (math.isnan (number)== True):
+        results=""
+        return render_template('result.html', results=results)
     # Write your to code here to check whether number is even or odd and render result.html page
 
 
 @app.get('/addStudentOrganisation')
 def displayStudentForm():
     # Complete this function to display studentFrom.html page
-    pass
+    return render_template('studentForm.html')
 
 
 @app.route('/addStudentOrganisation', methods=['POST'])
 def displayRegistrationPage():
     # Get student name and organisation from form.
     studentName = request.form['name']
+    studentOrganisation = request.form['organisation']
 
     # Append this value to studentOrganisationDetails
+    studentOrganisationDetails[studentName]= studentOrganisation
 
     # Display studentDetails.html with all students and organisations
+    return render_template('studentDetails.html', studentOrganisationDetails=studentOrganisationDetails)
